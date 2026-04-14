185edf5643affa98373467c81451c7f4f32afbd3
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-04-14 10:08:04 +0200

    Create author module and its endpoints

---
+++ src/authors/authors.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { AuthorsService } from './authors.service';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { CreateAuthorDTO } from './dtos/create-author.dto';
+import { UpdateAuthorDTO } from './dtos/update-author.dto';
+
+@Controller('authors')
+export class AuthorsController {
+  constructor(private authorsService: AuthorsService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.authorsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const auth = await this.authorsService.getById(id);
+    if (!auth) throw new NotFoundException('Author not found...');
+    return auth;
+  }
+
+  @Post('/')
+  create(@Body() authorData: CreateAuthorDTO) {
+    return this.authorsService.create(authorData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() authorData: UpdateAuthorDTO,
+  ) {
+    if (!(await this.authorsService.getById(id)))
+      throw new NotFoundException('Author not found');
+
+    await this.authorsService.updateById(id, authorData);
+    return { success: true };
+  }
+
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.authorsService.getById(id)))
+      throw new NotFoundException('Author not found');
+
+    await this.authorsService.deleteById(id);
+    return { success: true };
+  }
+}

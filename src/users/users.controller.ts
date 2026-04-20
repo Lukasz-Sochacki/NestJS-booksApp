97306e02c7b04f614baf154504f29c0d6290a9fc
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-04-20 13:22:06 +0200

    Add delete endpoints for users which have ADMIN role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import {
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('user')
 export class UsersController {
@@ -24,5 +28,15 @@ export class UsersController {
       throw new NotFoundException('User not found...');
     }
     return user;
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  public async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.userService.getById(id)))
+      throw new NotFoundException('User not found...');
+    await this.userService.deleteById(id);
+    return { success: true };
   }
 }
